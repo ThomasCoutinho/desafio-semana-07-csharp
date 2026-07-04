4475d4a4be329988158c2bf684f16da316f8a6e6
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-07-04 14:26:45 -0300

    Desafio 2 finalizado

---
+++ Desafio2ControleDeEstoque/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("============================================");
+Console.WriteLine("      Controle de Estoque de Produtos       ");
+Console.WriteLine("============================================");
+
+Console.Write("\nDigite o nome do produto: ");
+var nomeProduto = Console.ReadLine();
+
+Console.Write("Digite a quantidade atual: ");
+int quantAtual = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n============================================");
+
+if (quantAtual < 5)
+{
+    Console.WriteLine($"Estoque de {nomeProduto} crítico, necessário reposição urgente!");
+}
+else if (quantAtual >= 5 && quantAtual <= 10)
+{
+    Console.WriteLine($"Estoque de {nomeProduto} baixo, fique atento!");
+}
+else
+{
+    Console.WriteLine($"Estoque de {nomeProduto} OK!");
+}
+
+Console.WriteLine("============================================");
