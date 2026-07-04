c90ce2cae661290ccce59997fe12ae8a0b39754c
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-07-04 14:48:40 -0300

    Desafio 3 finalizado

---
+++ Desafio3FilaDeAtendimento/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("============================================");
+Console.WriteLine("            Fila de atendimento"            );
+Console.WriteLine("============================================");
+
+Queue<string> fila = new Queue<string>();
+
+fila.Enqueue("Maria");
+fila.Enqueue("João");
+fila.Enqueue("Pedro");
+
+string pacienteAtual = fila.Dequeue();
+
+Console.WriteLine($"\nPaciente em atendimento: {pacienteAtual}");
+Console.WriteLine("\n============================================");
+Console.WriteLine("\nFila de espera:");
+foreach (var paciente in fila)
+{
+    Console.WriteLine($" - {paciente}");
+}
+
+Console.WriteLine("\n============================================");
