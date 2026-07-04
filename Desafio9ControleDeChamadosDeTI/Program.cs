e39d1ea83831a449d66cd21ed45da15ec1690fda
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-07-04 17:51:53 -0300

    Desafio 9 finalizado

---
+++ Desafio9ControleDeChamadosDeTI/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("================================================");
+Console.WriteLine("             Controle de chamados               ");
+Console.WriteLine("              First In, First Out               ");
+Console.WriteLine("================================================");
+
+Queue<string> chamadosFila = new Queue<string>();
+
+chamadosFila.Enqueue("Impressora não funciona");
+chamadosFila.Enqueue("Computador lento");
+chamadosFila.Enqueue("Problema no e-mail");
+
+string chamadoAtual = chamadosFila.Dequeue();
+
+Console.WriteLine($"\nChamado em atendimento: {chamadoAtual}");
+Console.WriteLine("\n================================================");
+
+Console.WriteLine("\nChamados na fila:");
+
+foreach (var chamado in chamadosFila)
+{
+    Console.WriteLine($" - {chamado}");
+}
+
+Console.WriteLine("\n================================================");
+
