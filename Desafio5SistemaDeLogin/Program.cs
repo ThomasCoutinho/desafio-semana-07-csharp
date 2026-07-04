502740239265175a5a1a921e58f491cf843a9117
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-07-04 15:44:13 -0300

    Desafio 5 finalizado

---
+++ Desafio5SistemaDeLogin/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("============================================");
+Console.WriteLine("              Sistema de Login              ");
+Console.WriteLine("============================================");
+
+string login = "admin";
+string senha = "123456";
+
+Console.Write("\nDigite o usuário: ");
+string loginDig = Console.ReadLine();
+Console.Write("Digite a senha: ");
+string senhaDig = Console.ReadLine();
+Console.WriteLine("\n============================================");
+
+if (loginDig == login && senhaDig == senha)
+{
+    Console.WriteLine("Login realizado com sucesso!");
+}
+else
+{
+    Console.WriteLine("Usuário ou senha incorretos!");
+}
+Console.WriteLine("============================================");
